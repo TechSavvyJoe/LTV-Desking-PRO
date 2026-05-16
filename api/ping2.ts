fbbedbc8b062a12ef9a74d35bd6816abac380c4b
Joe Gallant <TechSavvyJoe@users.noreply.github.com>
2026-05-16 14:56:59 -0400

    test: same-dir import diagnostic

---
+++ api/ping2.ts
@@ -0,0 +1,8 @@
+import type { IncomingMessage, ServerResponse } from "node:http";
+import { greet } from "./ping-helper";
+
+export default function handler(_req: IncomingMessage, res: ServerResponse): void {
+  res.statusCode = 200;
+  res.setHeader("Content-Type", "application/json");
+  res.end(JSON.stringify({ ok: true, msg: greet() }));
+}
